0c2741bce6985f06376cbcfe2ae88195b7f439d9
Josh Bawtinheimer <jbawtinheimer@gmail.com>
2020-10-22 14:27:24 -0600

    initial commit

---
+++ express-back-end/server.js
@@ -0,0 +1,85 @@
+const Express = require("express");
+const App = Express();
+const BodyParser = require("body-parser");
+const PORT = 8080;
+
+const cookieSession = require("cookie-session");
+
+App.use(
+  cookieSession({
+    name: "session",
+    keys: ["secretKey"],
+  })
+);
+
+const { Pool } = require("pg");
+const dbParams = require("./lib/db.js");
+const db = new Pool(dbParams);
+db.connect(() => {
+  console.log("connected to database");
+});
+
+// Express Configuration
+App.use(BodyParser.urlencoded({ extended: false }));
+App.use(BodyParser.json());
+App.use(Express.static("public"));
+
+//home route
+App.get("/", (req, res) => {
+  res.json({
+    message: "database /",
+  });
+});
+
+// Sample GET route
+App.get("/api/data", (req, res) => {
+  res.json({
+    message: "Seems to work!",
+  });
+});
+
+// - GET /api/index
+App.get("/api/index", (req, res) => {
+  res.json({
+    message: "index",
+  });
+});
+// - GET /api/products
+
+App.get("/api/products", (req, res) => {
+  res.json({
+    message: "products",
+  });
+});
+// - GET /api/products/:id/prices
+App.get("/api/products/:id/prices", (req, res) => {
+  res.json({
+    message: "products/:id/prices",
+  });
+});
+// - GET /api/cart
+App.get("/api/cart", (req, res) => {
+  res.json({
+    message: "carts",
+  });
+});
+
+// - GET /api/users/:id
+App.get("/api/users/:id", (req, res) => {
+  res.json({
+    message: "users/:id",
+  });
+});
+
+App.post("/api/cart", (req, res) => {
+  res.json({
+    message: "cart",
+  });
+});
+
+App.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(
+    `Express seems to be listening on port ${PORT} so that's pretty good 👍`
+  );
+});
--- planning/README.md
+++ planning/README.md
@@ -1,1 +1,28 @@
-# CanadaBlinds
+## User stories
+
+- User can visit home-page/about/how-to/products from nav
+- User can select product and customize with provided options
+- User can add to cart
+- User can see cart and pay
+- User can see order go through
+- User can change quantity in cart
+
+## Stretch
+
+- User can see successful payment and order info
+- User can hover over product and see other demo images
+
+## Routes
+
+### GET
+
+- GET /api/index
+- GET /api/products
+- GET /api/products/:id/prices
+- GET /api/cart
+- GET /api/users/:id
+
+### POST
+
+- POST /api/cart
+- POST /api/users
