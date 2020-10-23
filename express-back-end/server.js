26b7bcdf47a1715d770491e0473b89c02b16ca0f
rafogi <raffycordero2001@yahoo.ca>
2020-10-23 11:31:51 -0700

    create category schema

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -2,8 +2,6 @@ const Express = require("express");
 const App = Express();
 const BodyParser = require("body-parser");
 const PORT = 3005;
-const usersRoute = require ('./routes/users')
-
 const cookieSession = require("cookie-session");
 
 App.use(
@@ -25,60 +23,14 @@ App.use(BodyParser.urlencoded({ extended: false }));
 App.use(BodyParser.json());
 App.use(Express.static("public"));
 
+// routes
+const usersRoute = require ('./routes/users');
+const categoriesRoute = require ('./routes/cateegories');
+
+// using routes
 App.use('/users', usersRoute(db));
+App.use('/categories', categoriesRoute(db));
 
-//home route
-App.get("/", (req, res) => {
-  res.json({
-    message: "database /",
-  });
-});
-
-// Sample GET route
-App.get("/api/data", (req, res) => {
-  res.json({
-    message: "Seems to work!",
-  });
-});
-
-// - GET /api/index
-App.get("/api/index", (req, res) => {
-  res.json({
-    message: "index",
-  });
-});
-// - GET /api/products
-
-App.get("/api/products", (req, res) => {
-  res.json({
-    message: "products",
-  });
-});
-// - GET /api/products/:id/prices
-App.get("/api/products/:id/prices", (req, res) => {
-  res.json({
-    message: "products/:id/prices",
-  });
-});
-// - GET /api/cart
-App.get("/api/cart", (req, res) => {
-  res.json({
-    message: "carts",
-  });
-});
-
-// - GET /api/users/:id
-App.get("/api/users/:id", (req, res) => {
-  res.json({
-    message: "users/:id",
-  });
-});
-
-App.post("/api/cart", (req, res) => {
-  res.json({
-    message: "cart",
-  });
-});
 
 App.listen(PORT, () => {
   // eslint-disable-next-line no-console
