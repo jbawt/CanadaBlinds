1dc636e38b2f2d5cd3eec51e3ba763ae369bda36
Ammar <shakirammar12@gmail.com>
2020-10-23 13:53:51 -0700

    fixed products table

--- express-back-end/db/schema/03_products.sql
+++ express-back-end/db/schema/03_products.sql
@@ -5,6 +5,6 @@ CREATE TABLE products (
   description VARCHAR(255) NOT NULL,
   thumbnail VARCHAR(255) NOT NULL,
   image VARCHAR(255) NOT NULL,
-  category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
+  category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE
 );
 
