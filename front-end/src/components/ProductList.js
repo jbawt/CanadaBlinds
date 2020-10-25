72bbe4778ce2be443c94aa2902be7e31ad18406b
Raphael Cordero <raffycordero2001@yahoo.ca>
2020-10-25 08:08:56 +0000

    change product img on db

--- front-end/src/components/ProductList.js
+++ front-end/src/components/ProductList.js
@@ -13,11 +13,15 @@ export default function ProductList(props) {
     <form className = "product">
       <header>
         <h4> {props.name} </h4>
-        <img src={props.image} onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}/>
+        <div onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}>
         {hover && (
           <img src={props.image2}/>
-      )}
+        )}
+        {!hover && (
+          <img src={props.image}/>
+        )}
+        </div>
       </header>
       <footer>
       <p>
