c30c9e94952e3c98906cc7a2e3afb1c933ab9c61
Josh Bawtinheimer <jbawtinheimer@gmail.com>
2020-10-27 12:12:34 -0600

    styling price box

--- front-end/src/components/Product-form/Price.js
+++ front-end/src/components/Product-form/Price.js
@@ -6,7 +6,11 @@ export default function Product({ price, optionPrice }) {
     <div className="price-total-container">
       <h1>Item Total</h1>
       <div className="price-info">
-        <h2>{price + optionPrice()}$</h2>
+        <h4>
+          Retail price: <span>${price + optionPrice()}</span>
+        </h4>
+        <h2>Your price: ${price / 2 + optionPrice()}</h2>
+        <h4>Total savings are ${price / 2}</h4>
       </div>
       <button type="submit">Add to cart</button>
     </div>
